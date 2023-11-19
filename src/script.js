b4cdfa2f86c3ce9702f95a7be04db39571c0bf28
BethJW <bethanyjwilliamson16@gmail.com>
2023-11-19 21:10:09 +0000

    Created search engine with API integration

--- src/script.js
+++ src/script.js
@@ -1,15 +1,23 @@
+function refreshWeather(response) {
+  let temperature = response.data.temperature.current;
+  let currentTemp = document.querySelector("#currentTemp");
+  currentTemp.innerHTML = Math.round(temperature);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `Todays' weather in ${response.data.city}`;
+}
+
 function search(city) {
   let apiKey = `b9b10f3af19o1ba6ec0aed0664cb453t`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-bar");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `Todays' weather in ${searchInput.value}`;
   search(searchInput.value);
 }
 let searchForm = document.querySelector("#search");
 searchForm.addEventListener("submit", submitSearch);
+
+search("Crewe");
