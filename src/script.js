414b5439e85913c7965e8c23e5d3a610fde3b13f
BethJW <bethanyjwilliamson16@gmail.com>
2023-11-19 22:09:41 +0000

    Injected more weather data with API

--- src/script.js
+++ src/script.js
@@ -4,6 +4,51 @@ function refreshWeather(response) {
   currentTemp.innerHTML = Math.round(temperature);
   let h1 = document.querySelector("h1");
   h1.innerHTML = `Todays' weather in ${response.data.city}`;
+  let WeatherDescription = document.querySelector("#description");
+  WeatherDescription.innerHTML = `Today you will have ${response.data.condition.description}`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
+  let wind = document.querySelector("#wind");
+  let windSpeed = Math.round(response.data.wind.speed);
+  wind.innerHTML = `Wind: ${windSpeed}km/h`;
+  let date = new Date(response.data.time * 1000);
+  let time = document.querySelector("#currentTime");
+  time.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let number = date.getDate();
+  let day = days[date.getDay()];
+  return `It is ${day}, ${number} ${month} and the time is ${hours}:${minutes}`;
 }
 
 function search(city) {
